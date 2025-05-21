41dc7c8f194ec1e56c5983d15205be333ca969db
Dawid Ratajczak <dawid.ratajczak06+github@gmail.com>
2025-05-21 19:53:32 +0200

    Added css support

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,32 @@
+use std::env::set_var;
+use std::fs::File;
+use std::io::Read;
+
 use gtk4::prelude::*;
 use gtk4::{Application, ApplicationWindow, Box as GtkBox, Label, Orientation};
 use gtk4_layer_shell::{Edge, Layer, LayerShell};
-use hypr_ipc::error::Result;
+use window::WindowManager;
 
 pub mod window;
 
-fn main() -> Result<()> {
+fn main() {
   unsafe {
-    std::env::set_var("GDK_BACKEND", "wayland");
+    set_var("GDK_BACKEND", "wayland");
   }
 
-  let app = Application::builder().application_id("com.example").build();
-  gtk4::init().expect("Failed to init");
-  println!("Supported: {}", gtk4_layer_shell::is_supported());
-  app.connect_activate(|app| {
-    let window = ApplicationWindow::builder()
-      .application(app)
-      .title("Panel")
-      .default_width(1920)
-      .default_height(30)
-      .build();
+  let mut file = File::open("./example.yml").expect("Failed to open file");
+  let window_manager = WindowManager::from_config(&mut file);
+  println!("Window manager: {:?}", window_manager);
 
-    window.set_decorated(false);
+  gtk4::init().expect("Failed to init gtk4");
 
-    let container = GtkBox::new(Orientation::Horizontal, 0);
-    container.append(&Label::new(Some("This is a fixed panel")));
+  let window = window_manager.window("test").unwrap();
 
-    window.set_child(Some(&container));
+  let mut css_file = File::open("./style.css").expect("Failed to open styles");
+  let mut buf = String::new();
+  css_file
+    .read_to_string(&mut buf)
+    .expect("Failed to read css");
 
-    window.init_layer_shell();
-    window.set_layer(Layer::Top);
-    window.auto_exclusive_zone_enable();
-    window.set_anchor(Edge::Top, true);
-    window.set_anchor(Edge::Right, true);
-    window.set_anchor(Edge::Left, true);
-    window.set_exclusive_zone(30);
-
-    window.show();
-  });
-
-  app.run();
-  Ok(())
+  window.open(&buf);
 }
